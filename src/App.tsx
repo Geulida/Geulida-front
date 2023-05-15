f4e79b9d4cdbc3fcb19fe67f8c5b99ab4a482414
shinhayeong <judy.hayoung.shin@gmail.com>
2023-05-16 01:36:50 +0900

    chore : react-reset 라이브러리 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Reset } from 'styled-reset';
 import './App.css';
 import ResultPage from './page/ResultPage';
 
 function App() {
   return (
     <BrowserRouter>
-      <Reset />
       <Routes>
-        <Route path="/result" element={<ResultPage />} />
+        <Route path='/result' element={<ResultPage />} />
       </Routes>
     </BrowserRouter>
   );
