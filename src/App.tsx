c3e0fc32f4ef506b11e491dcad4b45132fd98fec
shinhayeong <judy.hayoung.shin@gmail.com>
2023-05-17 02:46:38 +0900

    feat : colorPicker완

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import ResultPage from './pages/ResultPage';
+import ColorPickPage from './pages/ColorPickPage';
+import PainterPickPage from './pages/PainterPickPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/color-pick' element={<ColorPickPage />} />
+        <Route path='/painter-pick' element={<PainterPickPage />} />
         <Route path='/result' element={<ResultPage />} />
       </Routes>
     </BrowserRouter>
