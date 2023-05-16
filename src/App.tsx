67c3db9eb89c868499688af40a912bc6c47fb4ec
신혜지 <shinhj4068@naver.com>
2023-05-17 01:33:05 +0900

    style: 파일 경로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import ResultPage from './page/ResultPage';
+import ResultPage from './pages/ResultPage';
 
 function App() {
   return (
