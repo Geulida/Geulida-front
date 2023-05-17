c97f5ca309e955f9625b92459c020ce9bdc48f41
신혜지 <shinhj4068@naver.com>
2023-05-17 14:49:56 +0900

    feat: 메시지 창 구현

--- src/pages/ChatPage/index.tsx
+++ src/pages/ChatPage/index.tsx
@@ -3,7 +3,9 @@ import styles from './chatPage.module.scss';
 import Layout from '../../components/common/Layout';
 
 function ResultPage() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState<number>(0);
+  const [userMsg, setUserMsg] = useState<string>();
+  const [aiMsg, setAiMsg] = useState<string[]>();
 
   useEffect(() => {
     if (count < 10) {
@@ -16,17 +18,49 @@ function ResultPage() {
   // 진행bar 동적으로 width 설정
   const progressBarWidth = `${(count / 10) * 600}px`;
 
+  // 메시지 입력 이벤트
+  function handleChange() {
+    
+  }
+
+
+  // 보내기 버튼 클릭 이벤트
+  function handleClick() {
+    
+  }
+
   return (
     <Layout>
       <div className={styles.main__container}>
+
         <div className={styles.count__container}>
           <p className={styles.countText}>남은 대화 턴 수 ({count}/10)</p>
           <div className={styles.progressBar}>
             <div className={styles.countingBar} style={{ width: progressBarWidth }}></div>
           </div>
         </div>
+
         <div className={styles.chat__container}>
-          채팅 화면입니다
+          <div className={styles.message__container}>
+            <div>{userMsg}</div>
+          </div>
+
+          <div className={styles.input__cotainer}>
+            <input
+              className={styles.input}
+              placeholder='메시지를 입력하세요'
+              onChange={(event) => {
+                setUserMsg(event.target.value);
+              }}
+            />
+
+            <button 
+              className={styles.button} 
+              onClick={(event) => handleClick()}
+            >
+              <img className={styles.buttonImg} src='send.svg'/>
+            </button>
+          </div>
         </div>
       </div>
     </Layout>
