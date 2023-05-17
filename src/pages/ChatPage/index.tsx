2cbc32d7429dd29dc38a3c440c1924b9031ed64c
신혜지 <shinhj4068@naver.com>
2023-05-17 20:38:42 +0900

    feat: AI와 사용자의 채팅 구현

--- src/pages/ChatPage/index.tsx
+++ src/pages/ChatPage/index.tsx
@@ -1,63 +1,124 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styles from './chatPage.module.scss';
-import Layout from '../../components/common/Layout';
+import Layout from 'components/common/Layout';
 import { ReactComponent as Send } from 'assets/Send.svg';
 
-function ResultPage() {
-  const [count, setCount] = useState<number>(0);
-  const [userMsg, setUserMsg] = useState<string>();
-  const [aiMsg, setAiMsg] = useState<string[]>();
+interface Message {
+  id: number;
+  content: string;
+}
 
-  useEffect(() => {
-    if (count < 10) {
-      setTimeout(() => {
-        setCount(count + 1);
-      }, 1000);
-    }
-  }, [count]);
+function ChatPage() {
+  const [count, setCount] = useState<number>(0);
+  const [userMsg, setUserMsg] = useState<Message[]>([]);
+  const [aiMsg, setAiMsg] = useState<Message[]>([]);
+  const [inputValue, setInputValue] = useState<string>('');
+  const [isDisabled, setIsDisabled] = useState<boolean>(true);
+
+  // 최대 대화 가능 횟수
+  const maxCount = 10;
 
   // 진행bar 동적으로 width 설정
-  const progressBarWidth = `${(count / 10) * 600}px`;
-
-  // 메시지 입력 이벤트
-  function handleChange() {
-    
-  }
-
+  const progressBarWidth = `${(count / maxCount) * 600}px`;
 
   // 보내기 버튼 클릭 이벤트
   function handleClick() {
-    
+    setIsDisabled(true);
+    if (inputValue.trim() !== '' && count < maxCount) {
+      const newMessage: Message = {
+        id: userMsg.length + 1,
+        content: inputValue
+      };
+      setUserMsg([...userMsg, newMessage]);
+      setInputValue('');
+    }
+  }
+
+  useEffect(() => {
+    // 진행도 설정
+    if (count < maxCount) {
+      setCount(userMsg.length);
+    } 
+
+    // AI의 응답 생성 및 추가
+    if (userMsg.length === aiMsg.length && aiMsg.length > 0) {
+      setIsDisabled(true);
+      // 임시로 2초 후에 보내기 (disabled 확인용)
+      setTimeout(() => {
+        const newAiMessage: Message = {
+          id: aiMsg.length + 1,
+          content: generateAiResponse()
+        };
+        setAiMsg([...aiMsg, newAiMessage]);
+
+        // 마지막 AI 응답을 받은 후 입력창 막기
+        if (maxCount <= aiMsg.length) {
+          setIsDisabled(true);
+        } else {
+          setIsDisabled(false);
+        }
+
+      }, 2000);
+    }
+  }, [userMsg]);
+  
+  // 페이지가 시작되면 AI의 첫 번째 대화를 생성하여 보여줌
+  useEffect(() => {
+    if (aiMsg.length === 0 && userMsg.length === 0) {
+      const firstAiMessage: Message = {
+        id: 1,
+        content: "안녕 난 첫번째 메시지야"
+      };
+      setAiMsg([firstAiMessage]);
+      setIsDisabled(false);
+    }
+  }, []);
+
+  // AI의 응답 받아오기
+  function generateAiResponse() {
+    return "AI 대답을 여기에 저장해서 보여주기!!";
   }
 
   return (
     <Layout>
-      <div className={styles.main__container}>
+      <div className={styles.mainContainer}>
 
-        <div className={styles.count__container}>
-          <p className={styles.countText}>남은 대화 턴 수 ({count}/10)</p>
+        <div className={styles.countContainer}>
+          <p className={styles.countText}>남은 대화 턴 수 ({count}/{maxCount})</p>
           <div className={styles.progressBar}>
             <div className={styles.countingBar} style={{ width: progressBarWidth }}></div>
           </div>
         </div>
 
-        <div className={styles.chat__container}>
-          <div className={styles.message__container}>
-            <div>{userMsg}</div>
-          </div>
+        <div className={styles.chatContainer}>
 
-          <div className={styles.input__cotainer}>
+        <div className={styles.messageContainer}>
+          {aiMsg.map((message, index) => (
+            <div key={message.id} className={styles.msgWraaper}>
+              <p>&#x1F916; 그리다 AI</p>
+              <div className={styles.aiMsg}>{message.content}</div>
+              {userMsg[index] && (
+                <div className={styles.userMsg}>{userMsg[index].content}</div>
+              )}
+            </div>
+          ))}
+        </div>
+
+          <div className={styles.inputCotainer}>
             <input
               className={styles.input}
-              placeholder='메시지를 입력하세요'
-              onChange={(event) => {
-                setUserMsg(event.target.value);
-              }}
+              placeholder='메시지를 입력하세요 (최대 500자까지 작성할 수 있습니다)'
+              type="text"
+              value={inputValue}
+              maxLength={500}
+              onChange={(e) => setInputValue(e.target.value)}
+              disabled={isDisabled}
             />
 
             <button 
-              className={styles.button} 
-              onClick={(event) => handleClick()}
+              className={styles.button}
+              onClick={handleClick}
+              disabled={isDisabled}
             >
               <Send />
             </button>
@@ -68,4 +129,4 @@ function ResultPage() {
   );
 }
 
-export default ResultPage;
+export default ChatPage;
