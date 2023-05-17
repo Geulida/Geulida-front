a3968c70aba50c6daf85478d61ed103d82c0a65f
gwanseeum <9v.veu.v@gmail.com>
2023-05-17 15:50:32 +0900

    feat: collection 페이지 구현

--- src/pages/Collection/CollectionPage.tsx
+++ src/pages/Collection/CollectionPage.tsx
@@ -1,11 +1,34 @@
+import { useState } from 'react';
 import Headline from 'components/common/Headline';
 import styles from './CollectionPage.module.scss';
+import dummy from 'assets/dummyCollection.json';
 
 function CollectionPage() {
+  const [expandedItemId, setExpandedItemId] = useState<string | null>(null);
+
+  const toggleItem = (itemId: string): void => {
+    setExpandedItemId((prevItemId) => (prevItemId === itemId ? null : itemId));
+  };
+
   return (
     <div className={styles.container}>
-      <Headline title='Collection'></Headline>
-      <div className={styles.main}></div>
+      <Headline title='Collection' />
+      <div className={styles.main}>
+        {dummy.map((data) => (
+          <div key={data._id} className={styles.picDiv} onClick={() => toggleItem(data._id)}>
+            {expandedItemId === data._id ? (
+              <div className={styles.picDescription}>{data.summary}</div>
+            ) : (
+              <div
+                className={`${styles.pic}`}
+                style={{
+                  backgroundImage: `url(${data.result})`,
+                }}
+              />
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
