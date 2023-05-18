bf24dec75d78cc5192911852f96b10b65880d7b7
gwanseeum <9v.veu.v@gmail.com>
2023-05-19 03:51:36 +0900

    fix: 버튼 스타일 수정

--- src/pages/Collection/ItemDelete.tsx
+++ src/pages/Collection/ItemDelete.tsx
@@ -12,15 +12,15 @@ function ItemDelete(props: { bg: string }) {
   const hexCode = props.bg;
 
   function getBtnColor(hexCode: string) {
-    const percent = -20;
+    const percent = -17;
     const color = hexCode.replace('#', '');
 
     const r = parseInt(color.substring(0, 2), 16);
     const g = parseInt(color.substring(2, 4), 16);
     const b = parseInt(color.substring(4, 6), 16);
 
-    const adjustedR = Math.round((r * (100 + percent)) / 100);
-    const adjustedG = Math.round((g * (100 + percent)) / 100);
+    const adjustedR = Math.round((r * (100 + percent + 5)) / 100);
+    const adjustedG = Math.round((g * (100 + percent + 2)) / 100);
     const adjustedB = Math.round((b * (100 + percent)) / 100);
     const adjustedHex = ((adjustedR << 16) + (adjustedG << 8) + adjustedB).toString(16).padStart(6, '0');
 
