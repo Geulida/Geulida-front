4e17b29eb37a4d6837be2f1278a73f1d81a90b16
gwanseeum <9v.veu.v@gmail.com>
2023-05-19 03:52:56 +0900

    add: 로그인 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import styles from './App.module.scss';
 import ResultPage from './pages/ResultPage';
 import CollectionPage from './pages/Collection/CollectionPage';
 import Navi from './components/common/Navi';
+import Login from 'pages/Login/Login';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         </header>
         <div className={styles.main}>
           <Routes>
+            <Route path='/' element={<Login />} />
             <Route path='/result' element={<ResultPage />} />
             <Route path='/collection' element={<CollectionPage />} />
           </Routes>
