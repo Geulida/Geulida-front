62800251718fb1e34340e08be664cdc221683775
gwanseeum <9v.veu.v@gmail.com>
2023-05-19 02:18:12 +0900

    add: Collection 페이지 스타일 추가

--- src/pages/Collection/CollectionPage.tsx
+++ src/pages/Collection/CollectionPage.tsx
@@ -2,31 +2,15 @@ import { useState } from 'react';
 import Headline from 'components/common/Headline';
 import styles from './CollectionPage.module.scss';
 import dummy from 'assets/dummyCollection.json';
+import CollectionItem from './CollectionItem';
 
 function CollectionPage() {
-  const [expandedItemId, setExpandedItemId] = useState<string | null>(null);
-
-  const toggleItem = (itemId: string): void => {
-    setExpandedItemId((prevItemId) => (prevItemId === itemId ? null : itemId));
-  };
-
   return (
     <div className={styles.container}>
       <Headline title='Collection' />
       <div className={styles.main}>
         {dummy.map((data) => (
-          <div key={data._id} className={styles.picDiv} onClick={() => toggleItem(data._id)}>
-            {expandedItemId === data._id ? (
-              <div className={styles.picDescription}>{data.summary}</div>
-            ) : (
-              <div
-                className={`${styles.pic}`}
-                style={{
-                  backgroundImage: `url(${data.result})`,
-                }}
-              />
-            )}
-          </div>
+          <CollectionItem key={data._id} data={data} />
         ))}
       </div>
     </div>
