01ccef3546234abb91076cf9f532cd4ee53c0d4d
shinhayeong <judy.hayoung.shin@gmail.com>
2023-05-20 14:44:22 +0900

    fix : 로그인 모달 삭제 적용

--- src/components/common/Navi.tsx
+++ src/components/common/Navi.tsx
@@ -1,32 +1,9 @@
-import { useState, useRef, useEffect } from 'react';
 import styles from './Navi.module.scss';
 import { ReactComponent as Chat } from 'assets/Chat.svg';
 import { ReactComponent as Signup } from 'assets/Signup.svg';
 import { ReactComponent as Login } from 'assets/Login.svg';
-import Modal from './Modal';
-import modalStyles from './Modal.module.scss';
 
 function Navi() {
-  const [modalOpen, setModalOpen] = useState<boolean>(false);
-  const modalRef = useRef<HTMLDivElement | null>(null);
-
-  const handleModal = () => {
-    setModalOpen((open) => !open);
-  };
-
-  const handleModalOutside = (e: MouseEvent) => {
-    if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
-      setModalOpen(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener('mousedown', handleModalOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleModalOutside);
-    };
-  }, []);
-
   return (
     <div className={styles.nav}>
       <nav>
@@ -38,11 +15,7 @@ function Navi() {
             <Signup className={`${styles.btn} ${styles.top}`} />
           </div>
           <div>
-            <Login className={styles.btn} onClick={handleModal} />
-            {modalOpen && <div className={styles.backdrop} />}
-            <div className={`${modalStyles.overlay} ${modalOpen ? modalStyles.modalOpen : ''}`} ref={modalRef}>
-              <Modal onClose={() => setModalOpen(false)} />
-            </div>
+            <Login className={styles.btn} />
           </div>
         </div>
       </nav>
