46fa6fff91c4a89a8514679e5b74ff7dfd7e0f35
신혜지 <shinhj4068@naver.com>
2023-05-21 16:32:54 +0900

    design: 상태 끌어올리기 방식으로 수정

--- src/pages/ChatPage/ProgressBar.tsx
+++ src/pages/ChatPage/ProgressBar.tsx
@@ -1,23 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styles from './chatPage.module.scss';
 
 interface Props {
-  length: number;
   count: number;
-  setCount: (length: number) => void;
   maxCount: number;
 }
 
-function ProgressBar({ length, count, setCount, maxCount }: Props) {
+function ProgressBar({ count, maxCount }: Props) {
   // 진행bar 동적으로 width 설정
   const progressBarWidth = `${(count / maxCount) * 600}px`;
-
-  // 진행도 설정
-  useEffect(() => {
-    if (count < maxCount) {
-      setCount(length);
-    } 
-  }, [count, length]);
 
   return (
     <div className={styles.countContainer}>
