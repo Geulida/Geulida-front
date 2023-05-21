8a845a849ab2dd694b66dd9431a042ca977a6dd9
신혜지 <shinhj4068@naver.com>
2023-05-21 21:36:01 +0900

    feat: 세션 스토리지 저장 기능 구현

--- src/pages/ChatPage/index.tsx
+++ src/pages/ChatPage/index.tsx
@@ -19,14 +19,7 @@ function ChatPage() {
   const scrollRef = useRef<HTMLDivElement>(null);
 
   // 최대 대화 가능 횟수
-  const maxCount = 10;
-
-  // 진행도 설정
-  useEffect(() => {
-    if (count < maxCount) {
-      setCount(userMsg.length);
-    } 
-  }, [count, userMsg.length]);
+  const maxCount = 3;
 
   // 유저 메세지 입력 이벤트
   function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -54,29 +47,56 @@ function ChatPage() {
     return "AI 대답을 여기에 저장해서 보여주기!!";
   }
 
+  // 마지막 메시지 보낼 때 실행
+  function handleLastMessage(newAiMessage: Message[]) {
+    // 마지막으로 사용자가 메시지를 보내고 입력창 막기
+    maxCount <= userMsg.length ? setIsDisabled(true) : setIsDisabled(false);
+
+    // 마지막 AI 메세지(summary), 이미지 url 세션 스토리지에 저장
+    if (aiMsg.length === maxCount) {
+      const lastAiMessage = newAiMessage[newAiMessage.length - 1].content;
+      const getAnswerData = JSON.parse(sessionStorage.getItem('answerData') || '');
+      const answerData = { 
+        ...getAnswerData, 
+        summary: lastAiMessage,
+        url : "나중에 받아올 이미지 여기에 저장하기",
+      };
+
+      sessionStorage.setItem('answerData', JSON.stringify(answerData));
+    }
+  }
+
+  // 진행도 설정
+  useEffect(() => {
+    if (count < maxCount) {
+      setCount(userMsg.length);
+    } 
+  }, [count, userMsg.length]);
+
   // AI의 응답 생성 및 추가
   useEffect(() => {
     if (userMsg.length === aiMsg.length && aiMsg.length > 0) {
-      setIsDisabled(true);
       // 임시로 2초 후에 보내기 (disabled 확인용)
       setTimeout(() => {
         setAiMsg(() => {
           const newAiMessage = [...aiMsg];
           newAiMessage.push({
             id: aiMsg.length + 1,
-            content: generateAiResponse()
+            content: generateAiResponse() + aiMsg.length
           });
+
+          handleLastMessage(newAiMessage);
+
           return newAiMessage;
         });
-
-        // 마지막 AI 응답을 받은 후 입력창 막기
-        if (maxCount <= aiMsg.length) {
-          setIsDisabled(true);
-        } else {
-          setIsDisabled(false);
-        }
-
       }, 2000);
+    }
+  }, [userMsg, aiMsg]);
+
+  // 스크롤 위치 최신 대화에 맞도록
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
     }
   }, [userMsg, aiMsg]);
 
@@ -89,13 +109,6 @@ function ChatPage() {
     setAiMsg([firstAiMessage]);
     setIsDisabled(false);
   }, []);
-
-  // 스크롤 위치 최신 대화에 맞도록
-  useEffect(() => {
-    if (scrollRef.current) {
-      scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
-    }
-  }, [userMsg, aiMsg]);
 
   return (
     <Layout>
