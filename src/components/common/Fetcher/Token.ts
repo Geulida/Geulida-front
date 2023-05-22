464f95138008d1a51f55ebe741d518ff98f14d04
보살 <125162787+gwanseeum@users.noreply.github.com>
2023-05-22 14:15:41 +0900

    fix: 토큰 Api 삭제

--- src/components/common/Fetcher/Token.ts
+++ src/components/common/Fetcher/Token.ts
@@ -20,12 +20,4 @@ function removeToken(): void {
   document.cookie = 'token=; expires=Sun, 01 Jan 2023 00:00:00 UTC; path=/;';
 }
 
-// 토큰이 더 이상 유효하지 않을 때 호출해주세요.
-function invalidToken() {
-  removeToken();
-  alert('로그인이 유효하지 않습니다.\n다시 로그인 해주세요.');
-  const navigate = useNavigate();
-  navigate('/');
-}
-
-export { setToken, getToken, removeToken, invalidToken };
+export { setToken, getToken, removeToken };
