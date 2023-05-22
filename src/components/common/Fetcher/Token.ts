c6130af9fe87c97ea5c4dd706e3da760e708c348
보살 <125162787+gwanseeum@users.noreply.github.com>
2023-05-22 14:15:41 +0900

    fix: 토큰 Api 삭제

--- src/components/common/Fetcher/Token.ts
+++ src/components/common/Fetcher/Token.ts
@@ -1,5 +1,3 @@
-import { useNavigate } from 'react-router-dom';
-
 // 토큰을 쿠키에 저장합니다.
 function setToken(tokenValue: string) {
   document.cookie = `token=${tokenValue}; path=/`;
@@ -19,13 +17,5 @@ function getToken(): string | null {
 function removeToken(): void {
   document.cookie = 'token=; expires=Sun, 01 Jan 2023 00:00:00 UTC; path=/;';
 }
-
-// 토큰이 더 이상 유효하지 않을 때 호출해주세요.
-// function invalidToken() {
-//   removeToken();
-//   alert('로그인이 유효하지 않습니다.\n다시 로그인 해주세요.');
-//   const navigate = useNavigate();
-//   navigate('/');
-// }
 
 export { setToken, getToken, removeToken };
