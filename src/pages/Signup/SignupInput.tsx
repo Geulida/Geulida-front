ff3c467c1969f376b4d330ae66c1a1343acc24d0
gwanseeum <9v.veu.v@gmail.com>
2023-05-22 23:00:47 +0900

    Feat: 회원가입 유효성 검사

--- src/pages/Signup/SignupInput.tsx
+++ src/pages/Signup/SignupInput.tsx
@@ -5,15 +5,18 @@ interface Props {
   text: string;
   type: string;
   placeholder?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  error?: string;
 }
 
-function SignupInput({ name, text, type, placeholder }: Props) {
+function SignupInput({ name, text, type, placeholder, onChange, error }: Props) {
   return (
     <div>
       <label htmlFor={name} className={styles.label}>
         {text}
       </label>
-      <input type={type} placeholder={placeholder} id={name} className={styles.input} required />
+      <input type={type} placeholder={placeholder} id={name} className={styles.input} onChange={onChange} required />
+      {error && <span>{error}</span>}
     </div>
   );
 }
