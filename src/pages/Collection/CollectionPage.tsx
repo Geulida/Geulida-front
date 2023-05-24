d7593801d28d83ea89ea0b54a621d7daf9701e97
gwanseeum <9v.veu.v@gmail.com>
2023-05-24 22:52:24 +0900

    Fix: 페이지별 컴포넌트 수정

--- src/pages/Collection/CollectionPage.tsx
+++ src/pages/Collection/CollectionPage.tsx
@@ -1,16 +1,46 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Headline from 'components/common/Headline';
 import styles from './CollectionPage.module.scss';
 import dummy from 'assets/dummyCollection.json';
 import CollectionItem from './CollectionItem';
+import { userCollection } from 'components/common/Fetcher/Fetcher';
 
 function CollectionPage() {
+  const [collectionData, setCollectionData] = useState<any[]>([]);
+  const [page, setPage] = useState(1);
+
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
       behavior: 'smooth',
     });
   };
+
+  const loadCollection = async () => {
+    try {
+      const data: any = await userCollection(page);
+      setCollectionData((prevData) => [...prevData, ...data]);
+      setPage((prevPage) => prevPage + 1);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleScroll = () => {
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+    if (scrollTop + clientHeight >= scrollHeight - 100) {
+      loadCollection();
+    }
+  };
+
+  useEffect(() => {
+    loadCollection();
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
     <div className={styles.container}>
