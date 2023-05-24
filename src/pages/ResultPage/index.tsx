71dcaf91d1c8e5550ed1d009cb08417f5d6910bb
shinhayeong <judy.hayoung.shin@gmail.com>
2023-05-25 01:32:05 +0900

    feat : 결과 저장 API 연동 테스트

--- src/pages/ResultPage/index.tsx
+++ src/pages/ResultPage/index.tsx
@@ -1,5 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './resultPage.module.scss';
 
+import Modal from 'components/common/Modal';
 import KakaoShareBtn from '../../components/result/KakaoShareBtn';
 import GallerySaveBtn from '../../components/result/GallerySaveBtn';
 import ImageSaveBtn from '../../components/result/ImageSaveBtn';
@@ -13,10 +16,57 @@ import ImageSaveBtn from '../../components/result/ImageSaveBtn';
  * 3. 갤러리 저장 버튼이 컴포넌트로 구현되어있으니, 여기서 api 부착하고 button 클릭 이벤트 props로 gellerysavebtn에 전달한다.
  *
  */
+// const storedData = sessionStorage.getItem('answerData');
+interface data {
+  color: string;
+  hexcode: string;
+  style: string;
+  summary: string;
+  url: string;
+}
 
 function ResultPage() {
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [storedData, setStoredData] = useState<data>({
+    color: '',
+    hexcode: '',
+    style: '',
+    summary: '',
+    url: '',
+  });
+  const navigate = useNavigate();
+
+  const summary = 'summary';
+  const url = 'https://cdn.veritas-a.com/news/photo/old/2/admin_1198640645.jpg';
+
+  useEffect(() => {
+    try {
+      const getAnswerData = JSON.parse(sessionStorage.getItem('answerData') || '');
+      setStoredData({ ...storedData, color: getAnswerData.color, hexcode: getAnswerData.hexcode, style: getAnswerData.style, summary: summary, url: url });
+      console.log(getAnswerData);
+    } catch (err) {
+      console.error('Json에 유효한 값이 들어오지 않음');
+      handleModalShow();
+      console.log(showModal);
+    }
+  }, []);
+
+  function handleModalShow() {
+    setShowModal((prev) => !prev);
+  }
+
   return (
     <div className={styles.resultPage}>
+      {showModal && (
+        <Modal
+          modalType='navigate'
+          modalHandler={handleModalShow}
+          modalMessage='먼저 컬러와 화풍을 선택해주세요.'
+          navigateHandler={() => {
+            navigate('/color-pick');
+          }}
+        />
+      )}
       <div className={styles.resultPageContainer}>
         <section className={styles.resultContents}>
           <div className={styles.imageWrapper}>
@@ -26,7 +76,7 @@ function ResultPage() {
         </section>
         <section className={styles.resultBtnContainer}>
           <ImageSaveBtn />
-          <GallerySaveBtn />
+          <GallerySaveBtn storedData={storedData} />
           <KakaoShareBtn />
         </section>
       </div>
