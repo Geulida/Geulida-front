ce9a63ca8c4d8d9e7c629feb8b0dab87a25e1477
shinhayeong <judy.hayoung.shin@gmail.com>
2023-05-25 01:33:09 +0900

    fix : 혹시 몰라 추가했던 rgb사용 코드 끝에 오니  무쓸모라 지우고, convert 라이브러리 버그 고침

--- src/pages/ColorPickPage/index.tsx
+++ src/pages/ColorPickPage/index.tsx
@@ -1,9 +1,10 @@
 import { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
+
+import { ChromePicker, ColorResult } from 'react-color';
 import { GetColorName } from 'hex-color-to-color-name';
 
 import Modal from 'components/common/Modal';
-import { ChromePicker, ColorResult, RGBColor } from 'react-color';
 
 import styles from './colorPickPage.module.scss';
 import { ReactComponent as RightArrow } from 'assets/RightArrow.svg';
@@ -12,25 +13,15 @@ function ColorPickPage() {
   const [isShown, setIsShown] = useState<boolean>(false);
   const [showModal, setShowModal] = useState<boolean>(false);
   const [colorPickerHex, setColorPickerHex] = useState<string>('');
-  const [namedColor, setNamedColor] = useState('');
-  const [colorPickerRGB, setColorPickerRGB] = useState({
-    r: 255,
-    g: 255,
-    b: 255,
-    a: 100,
-  });
+  const [namedColor, setNamedColor] = useState<string>('');
+
   const navigate = useNavigate();
   const colorPickerRef = useRef<HTMLDivElement>(null);
 
-  const { r, g, b, a } = colorPickerRGB;
-
   const handleChangeColorPicker = (color: ColorResult) => {
-    setColorPickerRGB({ ...colorPickerRGB, ...(color.rgb as RGBColor) });
     setColorPickerHex(color.hex);
-    const standardName = GetColorName(colorPickerHex);
+    const standardName = GetColorName(color.hex);
     setNamedColor(standardName);
-    console.log(colorPickerHex);
-    console.log(namedColor);
   };
 
   const handleClickOutside = (e: MouseEvent) => {
@@ -67,14 +58,14 @@ function ColorPickPage() {
 
   return (
     <div className={styles.container}>
-      {showModal && <Modal modalType='color' modalHandler={handleModalShow} modalMessage='원하는 컬러를 선택해주세요' />}
+      {showModal && <Modal modalType='choice' modalHandler={handleModalShow} modalMessage='원하는 컬러를 선택해주세요' />}
       <div className={styles.left}></div>
       <div className={styles.colorPickerWrapper} ref={colorPickerRef}>
         {isShown && <ChromePicker className={styles.colorPicker} onChange={handleChangeColorPicker} color={colorPickerHex} />}
         <div
           className={styles.colorPickerCircle}
           style={{
-            backgroundColor: `rgba(${r},${g},${b},${a})`,
+            backgroundColor: `${colorPickerHex}`,
           }}
           onClick={() => setIsShown((prev) => !prev)}
         >
@@ -82,7 +73,7 @@ function ColorPickPage() {
         </div>
 
         <div className={styles.question1}>
-          Q1. 원하는 <span style={{ color: colorPickerHex }}>컬러</span>를 선택해주세요
+          Q1. 원하는<span style={{ color: `${colorPickerHex}` }}>컬러</span>를 선택해주세요
         </div>
       </div>
 
