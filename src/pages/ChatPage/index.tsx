d5e13ef5f86b2e13a8b54498be7fe803af5c4595
신혜지 <shinhj4068@naver.com>
2023-05-25 10:33:04 +0900

    feat: 모달 수정에 맞게 로직 변경

--- src/pages/ChatPage/index.tsx
+++ src/pages/ChatPage/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './chatPage.module.scss';
 import Layout from 'components/common/Layout';
+import Modal from 'components/common/Modal';
 import ProgressBar from './ProgressBar';
 import MessageContainer from './MessageContainer';
 import InputContainer from './InputContainer';
@@ -12,6 +13,14 @@ export interface Message {
   content: string;
 }
 
+interface data {
+  color: string;
+  hexcode: string;
+  style: string;
+  summary: string;
+  url: string;
+}
+
 function ChatPage() {
   const [count, setCount] = useState<number>(0);
   const [userMsg, setUserMsg] = useState<Message[]>([]);
@@ -19,12 +28,20 @@ function ChatPage() {
   const [inputValue, setInputValue] = useState<string>('');
   const [isDisabled, setIsDisabled] = useState<boolean>(true);
   const [showSpinner, setShowSpinner] = useState<boolean>(false);
+  const [showModal, setShowModal] = useState<boolean>(false);
   const scrollRef = useRef<HTMLDivElement>(null);
+  const [storedData, setStoredData] = useState<data>({
+    color: '',
+    hexcode: '',
+    style: '',
+    summary: '',
+    url: '',
+  });
 
   const navigate = useNavigate();
 
   // 최대 대화 가능 횟수
-  const maxCount = 10;
+  const maxCount = 1;
 
   // 유저 메세지 입력 이벤트
   function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -76,15 +93,38 @@ function ChatPage() {
 
   // 마지막 AI 메세지(summary), 이미지 url 세션 스토리지에 저장
   function handleLastMessage() {
-    const getAnswerData = JSON.parse(sessionStorage.getItem('answerData') || '');
-    const answerData = { 
-      ...getAnswerData, 
-      summary: summaryAiResponse(),
-      url : generateImageUrl(),
-    };
+    setStoredData((prevData) => {
+      const updatedData = {
+        ...prevData,
+        summary: summaryAiResponse(),
+        url: generateImageUrl(),
+      };
 
-    sessionStorage.setItem('answerData', JSON.stringify(answerData));
+      sessionStorage.setItem('answerData', JSON.stringify(updatedData));
+
+      return updatedData;
+    });
   }
+
+  // 모달 핸들 함수
+  function handleModalShow() {
+    setShowModal((prev) => !prev);
+  }
+
+  // 처음에 세션 스토리지 값 유효한지
+  useEffect(() => {
+    try {
+      const getAnswerData = JSON.parse(sessionStorage.getItem('answerData') || '');
+      setStoredData((prevData) => ({
+        ...prevData,
+        color: getAnswerData.color,
+        hexcode: getAnswerData.hexcode,
+        style: getAnswerData.style,
+      }));
+    } catch (err) {
+      handleModalShow();
+    }
+  }, []);
 
   // 진행도 설정
   useEffect(() => {
@@ -95,8 +135,11 @@ function ChatPage() {
 
   // AI의 응답 생성 및 추가
   useEffect(() => {
+    // 빈 값이면 메세지 요청 안 보냄
+    const isNullAnswerData = storedData.color === '' || storedData.hexcode === '' || storedData.style === '';
+
     // 임시로 2초 후에 보내기 (disabled 확인용)
-    if (userMsg.length === aiMsg.length) {
+    if (userMsg.length === aiMsg.length && !isNullAnswerData) {
       setTimeout(() => {
         handleAddAiResponse(generateAiResponse());
         setIsDisabled(false);
@@ -112,7 +155,7 @@ function ChatPage() {
         handleLastMessage();
       }, 2000);
     }
-  }, [userMsg, aiMsg]);
+  }, [userMsg, aiMsg, storedData]);
 
   // 스크롤 위치 최신 대화에 맞도록
   useEffect(() => {
@@ -121,17 +164,18 @@ function ChatPage() {
     }
   }, [userMsg, aiMsg]);
 
-  // 시작 전 세션 스토리지 값 유효한지
-  useEffect(() => {
-    if (!sessionStorage.getItem('answerData')) {
-      alert('먼저 원하는 컬러와 화풍을 선택해주세요!');
-      navigate('/color-pick');
-    }
-  }, []);
-
   return (
     <Layout>
       <div className={styles.mainContainer}>
+        {showModal && 
+          <Modal 
+            modalType='navigate'
+            modalHandler={handleModalShow} 
+            modalMessage='원하는 컬러와 화풍을 선택해주세요'
+            navigateHandler={() => {
+              navigate('/color-pick');
+            }}
+          />}
         <ProgressBar 
           count={count} 
           maxCount={maxCount}
