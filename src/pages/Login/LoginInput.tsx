8bbe97e5431dd5c6ca4a9de813c9315a724ee65c
gwanseeum <9v.veu.v@gmail.com>
2023-05-26 01:24:46 +0900

    fix: login페이지 api 연동

--- src/pages/Login/LoginInput.tsx
+++ src/pages/Login/LoginInput.tsx
@@ -3,12 +3,14 @@ import styles from './LoginInput.module.scss';
 interface Props {
   type: string;
   placeholder: string;
+  name: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-function LoginInput({ type, placeholder }: Props) {
+function LoginInput({ type, placeholder, onChange, name }: Props) {
   return (
     <div>
-      <input className={styles.input} type={type} placeholder={placeholder} required />
+      <input className={styles.input} type={type} placeholder={placeholder} name={name} onChange={onChange} required />
     </div>
   );
 }
